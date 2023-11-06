95f48cf8b88c9af23a2fb7c8ab1f342dc6d08460
Mikel Garcia <mikausin@gmail.com>
2023-11-06 19:28:57 +0100

    Initial commit.

---
+++ src/main/java/com/example/currencyconversionservice/CurrencyConversion.java
@@ -0,0 +1,57 @@
+package com.example.currencyconversionservice;
+
+
+import java.math.BigDecimal;
+
+public class CurrencyConversion {
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal quantity;
+    private BigDecimal conversionMultiple;
+    private BigDecimal totalCalculatedAmount;
+    private String environment;
+
+    public CurrencyConversion() {
+    }
+
+    public CurrencyConversion(Long id, String from, String to, BigDecimal quantity, BigDecimal conversionMultiple, BigDecimal totalCalculatedAmount, String environment) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversionMultiple = conversionMultiple;
+        this.quantity = quantity;
+        this.totalCalculatedAmount = totalCalculatedAmount;
+        this.environment = environment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public BigDecimal getConversionMultiple() {
+        return conversionMultiple;
+    }
+
+    public BigDecimal getQuantity() {
+        return quantity;
+    }
+
+    public BigDecimal getTotalCalculatedAmount() {
+        return totalCalculatedAmount;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+
+}
